2df9e7cd09105ad2242299fa8b69f78a28b56167
icekomar <miha.komar@mail.ru>
2024-09-10 21:16:54 +0300

    full lab1

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -2,6 +2,7 @@ import cv2 as cv
 import numpy as np
 
 if __name__ == '__main__':
+
     img = np.zeros((400, 400, 3), dtype="uint8")
     cv.imshow('df', img)
     cv.waitKey(1000)
@@ -23,6 +24,36 @@ if __name__ == '__main__':
 
     img = np.zeros((400, 400, 3), dtype="uint8")
     cv.rectangle(img, (15, 20), (70, 50), color=(0,255,255))
+    cv.imshow('df', img)
+    cv.waitKey(1000)
+
+
+    polygonePts = np.array([[110, 350], [130, 350],
+                            [150, 200], [110, 200], [110, 100],
+                            [140, 100], [140, 150], [170, 150],
+                            [170, 100], [200, 100], [200, 150],
+                            [230, 150], [230, 100], [260, 100],
+                            [260, 200], [220, 200], [240, 350],
+                            [260, 350], [260, 390], [110, 390]])
+
+    img = np.zeros((400, 400, 3), dtype="uint8")
+    cv.fillPoly(img, [polygonePts], color=(255, 255, 255))
+    cv.imshow('df', img)
+    cv.waitKey(1000)
+
+    img = np.zeros((400, 400, 3), dtype="uint8")
+    cv.polylines(img, [polygonePts], isClosed=True, color=(255, 255, 255))
+    cv.imshow('df', img)
+    cv.waitKey(1000)
+
+    img = np.zeros((400, 400, 3), dtype="uint8")
+    cv.polylines(img, [polygonePts], isClosed=False, color=(255, 255, 255))
+    cv.imshow('df', img)
+    cv.waitKey(1000)
+
+    img = np.zeros((400, 400, 3), dtype="uint8")
+    cv.putText(img, "Hello, word!", (20, 375), cv.FONT_ITALIC, 1, (255, 0, 255), 1)
+    cv.putText(img, "Hello, word!", (80, 50), cv.FONT_HERSHEY_SCRIPT_COMPLEX, 1, (0, 255, 255), 1)
     cv.imshow('df', img)
     cv.waitKey(1000)
 
